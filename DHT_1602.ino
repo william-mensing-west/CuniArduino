3f92586f263965173136454b62d8663426b147dd
wwest3204 <wwest3204@gmail.com>
2020-08-07 16:10:10 -0400

    Initial Commit

---
+++ DHT_1602.ino
@@ -0,0 +1,122 @@
+/*
+This sketch is used to collect temperature and humidity values from a DHT 111 sensor
+and display the values on a 1602 LCD display. The temperature and humidity is also 
+sent out the serial port to interface with a Jetson Nano.  The Nano has an AWS IoT
+device that is monitoring the serial port and then updating the device shadow to 
+interact with Alexa Skill voice commands.
+
+MIT License
+
+Original (C) 2015 Ole Wolf <wolf@blazingangles.com>
+Modified Copyright (c) 2020 William West
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+  The circuit:
+ * LCD RS pin to digital pin 7
+ * LCD Enable pin to digital pin 8
+ * LCD D4 pin to digital pin 9
+ * LCD D5 pin to digital pin 10
+ * LCD D6 pin to digital pin 11
+ * LCD D7 pin to digital pin 12
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K variable resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ * 
+ */
+
+#include <dht_nonblocking.h>
+#include <LiquidCrystal.h>
+
+// Sensor type definition
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+
+static const int DHT_SENSOR_PIN = 2;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+/*
+ * Initialize the serial port.
+ */
+void setup( )
+{
+  Serial.begin( 9600);
+  
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  
+  // Print a message to the LCD.
+  lcd.print("Hello Cuni!");
+}
+
+/*
+ * Poll for a measurement, keeping the state machine alive.  Returns
+ * true if a measurement is available.
+ */
+static bool measure_environment( float *temperature, float *humidity )
+{
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if( millis( ) - measurement_timestamp > 3000ul )
+  {
+    if( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      measurement_timestamp = millis( );
+      return( true );
+    }
+  }
+
+  return( false );
+}
+
+/*
+ * Main program loop.
+ */
+void loop( )
+{
+  float temperature;
+  float humidity;
+
+  /* Measure temperature and humidity.  If the functions returns
+     true, then a measurement is available. */
+  if( measure_environment( &temperature, &humidity ) == true )
+  {
+    Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+
+    lcd.setCursor(0, 0);
+    lcd.print("Temp = ");
+    lcd.print(temperature * 9 / 5 + 32);
+    lcd.print(" F ");
+
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1); 
+    lcd.print("Humidity = ");
+    lcd.print(humidity);
+    lcd.print(" % ");
+  }
+}
